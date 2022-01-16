2d2c0beac98e75ee7154dce43862d5a2c06f2da9
klaudiaKlebba <82536207+klaudiaKlebba@users.noreply.github.com>
2022-01-16 08:53:35 +0100

    Create Divide.java

---
+++ LAB4/src/main/java/Divide.java
@@ -0,0 +1,14 @@
+public class Divide {
+
+    public int a,b;
+
+    public int divide(int a, int b){
+        if(b==0){
+            return -1;
+        }
+        else{
+            return a/b;
+        }
+
+    }
+}
